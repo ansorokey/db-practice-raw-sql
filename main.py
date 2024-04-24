779a5666ef0cf90e8794beebd7a3f7a12f19f393
ansorokey <ansorokey@gmail.com>
2024-04-24 09:59:46 -0400

    basic menu loop

--- main.py
+++ main.py
@@ -3,4 +3,18 @@
 def main():
     print('Hello World')
 
+    runProgram = True
+    while runProgram == True:
+
+        print('MAIN MENU')
+        print('What would you like to do?')
+        print('Q. Quit')
+
+        response = input();
+        if response == 'q' or response == 'Q':
+            print('Tank you for using the database browser.')
+            runProgram = False
+        else:
+            print('I don\'t know that command')
+
 main()
